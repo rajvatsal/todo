af29492975c42be16a043cf92c13884f3163fa66
Vatsal <rajvatsal2004@gmail.com>
2024-04-21 07:03:51 +0530

    feat: Add taskmanager

--- src/scripts/logic.js
+++ src/scripts/logic.js
@@ -2,6 +2,7 @@ import { compareAsc, format } from "date-fns";
 
 const projects = [];
 
+// [ INTERFACES ]
 const inNOutInterface = (state) => ({
 	type: "inNOutInterface",
 	add: (options) => state.add(options, state),
@@ -12,10 +13,33 @@ const fetchInterface = (state) => ({
 	fetch: () => state.fetch(state),
 });
 
+// [ FACTORIES ]
+const TaskManager = (options) => {
+	const proto = {
+		tasks: [],
+		add: (opts, arg) => {
+			opts.selfIndex = arg.tasks.length;
+			arg.tasks.push(opts);
+		},
+		remove: (ptn, arg) => {
+			for (let i = 0; i < arg.tasks.length; i++) {
+				if (arg.tasks[i].name === ptn) arg.tasks.splice(i, 1);
+			}
+		},
+		fetch: (arg) => arg.tasks.slice(),
+	};
+
+	const inNOut = inNOutInterface(proto);
+	const fetch = fetchInterface(proto);
+	const composite = Object.assign(inNOut, fetch);
+	return Object.assign(Object.create(composite), options);
+};
+
 export const projectManager = ((options) => {
 	const proto = {
 		add: (opts) => {
 			opts.selfIndex = projects.length;
+			opts.taskManager = TaskManager();
 			projects.push(opts);
 		},
 		remove: (ptn) => {
@@ -33,4 +57,13 @@ export const projectManager = ((options) => {
 })();
 
 projectManager.add({ name: "vatsal", color: "charcoal" });
-console.log(projectManager.fetch());
+projectManager.add({ name: "anhay", color: "red" });
+projects[0].taskManager.add({
+	name: "Todo list",
+	desc: "Write the logic for todo list",
+	priority: "high",
+});
+projects[1].taskManager.add({ name: "Shananigans", desc: "jindagi jhand hai" });
+console.log(projects);
+console.log(projects[1].taskManager.fetch());
+console.log(projects[0].taskManager.fetch());
