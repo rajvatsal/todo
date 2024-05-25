92c7a1fa2cfe934262ca8fa8c176ceac832162ef
Vatsal <rajvatsal2004@gmail.com>
2024-05-25 20:20:11 +0530

    refactor(App): Use div.content to append to body

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -5,18 +5,35 @@ import AddProject from "./AddProjectDialog.js";
 import EditProject from "./FormEditProject.js";
 import { emit, off, on } from "./pub-sub";
 import { $, createElement } from "./utility";
-import { isValidProject } from "./logic";
 
-function render(projectList) {
+const body = $("body");
+
+const render = (projectList) => {
+	//conponents
 	const header = Header();
 	const dialogEditProject = EditProject();
 	const dialogAddProject = AddProject();
 	const sidebarComponent = Sidebar(projectList);
 	const main = Main();
-	$("body").prepend(sidebarComponent, dialogAddProject, dialogEditProject);
-	$(".r-side").append(header, main);
 
+	const content = createElement("div", { attributes: { class: "content" } });
+	const rightSide = createElement("div", { attributes: { class: "r-side" } });
+
+	rightSide.append(header, main);
+	content.prepend(
+		sidebarComponent,
+		dialogAddProject,
+		dialogEditProject,
+		rightSide,
+	);
+
+	return content;
+};
+
+const init = (projectList) => {
+	const content = render(projectList);
+	body.appendChild(content);
 	emit("openedMyProjects", projectList);
-}
+};
 
-on("renderApp", render);
+on("renderApp", init);
